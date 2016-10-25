572c06562c632bc66e352ffe725325ae54cfcff1
Luke Philips and Tracie Weitzman <tracie@>
2016-10-25 10:52:28 -0700

    create logic and convert array characters to dashes, convert array back to string and display

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,44 @@
 var stringInput = "";
 var arrayInput = [];
+var dashedArray = [];
+var dashedString = "";
+
+var vowelTest = function(letter) {
+  letter = letter.toLowerCase();
+  return letter != "a" &&
+  letter != "e" &&
+  letter != "i" &&
+  letter != "o" &&
+  letter != "u";
+};
 
 $(document).ready(function() {
-
   $("form").submit(function(event){
 
     stringInput = $("input#userSentence").val();
 
-    for (var i = 0; i < stringInput.length; i++) {
+    for (var i = 0; i < stringInput.length; i++) {// convert string to array
         arrayInput.push(stringInput[i]);
-    }
+    };
+    console.log(arrayInput);
 
+    dashedArray = arrayInput.map(function(letter) {
+      if (vowelTest(letter)){
+        return letter;
+      }
+      else {
 
-    alert(arrayInput);
-
-
+        return "-";
+      }
+    });
+    dashedString = dashedArray.join('');
+    $("#output").append(dashedString);
+    $("#output").show();
+    $("#input").hide();
     event.preventDefault();
   });
+  $("button#restart").click(function() {
+    location.reload();
+  });
+
 });
